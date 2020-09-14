236f52957885c97e28710d6cfcdb863db8cfa29b
gyur2 <gyur2@kookmin.ac.kr>
2020-09-14 11:45:59 +0900

    Create 05P12
    05_practice_2.ino

---
+++ 05P12/05_practice_2.ino.ino
@@ -0,0 +1,20 @@
+#define PIN_LED 7
+unsigned int count, toggle;
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+  Serial.begin(115200); // Initialize serial port
+  digitalWrite(PIN_LED, 0); // turn off LED.
+  delay(1000);
+}
+
+void loop() {
+  digitalWrite(PIN_LED, 1); // update LED status.
+  delay(300); // wait for 1,000 milliseconds
+  digitalWrite(PIN_LED, 0); // update LED status.
+  delay(300); // wait for 1,000 milliseconds
+}
+
+int toggle_state(int toggle) {
+  return toggle;
+}
